7fabd6758c027581e3592ff5719b568417fc2818
mateo <mgjini21@epoka.edu.al>
2023-11-29 14:33:29 +0100

    Added Manager Test but need to add more to it

--- src/test/java/Staff/ManagerTest.java
+++ src/test/java/Staff/ManagerTest.java
@@ -1,12 +1,5 @@
 package test.java.Staff;
 
-import Staff.Gender;
-import Staff.Manager;
-import Staff.Worker;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ManagerTest {
 
 }
