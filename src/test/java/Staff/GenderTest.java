5f1896ad68313c0e6a364a2b56068903c94f4e65
mateo <mgjini21@epoka.edu.al>
2023-11-29 16:30:31 +0100

    Added different tests

--- src/test/java/Staff/GenderTest.java
+++ src/test/java/Staff/GenderTest.java
@@ -1,4 +1,35 @@
 package test.java.Staff;
 
+import Staff.Gender;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GenderTest {
+    @Test
+    void testEnumConstants() {
+        assertEquals(2, Gender.values().length);
+        assertEquals(Gender.MALE, Gender.valueOf("MALE"));
+        assertEquals(Gender.FEMALE, Gender.valueOf("FEMALE"));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("MALE", Gender.MALE.toString());
+        assertEquals("FEMALE", Gender.FEMALE.toString());
+    }
+
+    @Test
+    void testEquality() {
+        assertNotEquals(Gender.MALE, Gender.FEMALE);
+        assertEquals(Gender.MALE, Gender.MALE);
+        assertEquals(Gender.FEMALE, Gender.FEMALE);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(Gender.MALE.hashCode(), Gender.MALE.hashCode());
+        assertEquals(Gender.FEMALE.hashCode(), Gender.FEMALE.hashCode());
+        assertNotEquals(Gender.MALE.hashCode(), Gender.FEMALE.hashCode());
+    }
 }
